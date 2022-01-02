76396d5691793e00b1cf7975766ead3b485ca3e4
MrFufux <anfefu1@gmail.com>
2022-01-02 17:02:50 -0500

    Created the fetching function and the endpoint for the API to get data

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,10 @@
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <Link to="/">
+            </nav>
+        </header>
+    )
+}
---
+++ src/context/UserProvider.jsx
@@ -0,0 +1,2 @@
+import { createContext,useState, useEffect } from "react";
+import {getUser} from ".../utils/request";
